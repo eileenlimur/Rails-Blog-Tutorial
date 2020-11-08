934e9da8c7301dea9937f2b6448ee1b19b3894a1
Eileen Li <eileenlimur@gmail.com>
2020-11-08 18:38:37 -0500

    add validation to posts and application controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if :advise_controller?
+  
+  #sign in and sign out
+  
+  protected
+  
+  def configure_permitted_parameters 
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
 end
